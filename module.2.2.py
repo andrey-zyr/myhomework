e3ad940cb80db58035fed43c2747a40948752b58
Андрей <zaan@inbox.ru>
2025-01-23 17:38:42 +0500

    my homework

---
+++ module.2.2.py
@@ -0,0 +1,8 @@
+first = int (input("first:"))
+second = int (input("second:"))
+third = int (input("third:"))
+if first == second and second == third:
+    print("3")
+elif first == second or second == third or first == third:
+    print("2")
+else: print("0")
